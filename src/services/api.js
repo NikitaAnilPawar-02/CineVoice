17b7faeaff7de4961e2c282d2c51617686bfd0ff
NikitaAnilPawar-02 <pawarnikita0293@gmail.com>
2025-11-22 22:39:39 +0530

    Update API config and gitignore

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "dc94517a9057eddb5a479530ae4afe06";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
